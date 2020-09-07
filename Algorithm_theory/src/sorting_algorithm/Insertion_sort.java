f416a3d3df279acf4361606ab89ef5c3dfe2d4f7
Jin-Woo Kim <dnehd6739@naver.com>
2020-09-07 22:23:12 +0900

    insertion sorting example

---
+++ Algorithm_theory/src/sorting_algorithm/Insertion_sort.java
@@ -0,0 +1,23 @@
+package sorting_algorithm;
+
+public class Insertion_sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] data = new int[]{5,2,9,3,1,4,6};
+		
+		for(int i=1;i<data.length;i++) {
+			for(int j=i;j>0;j--) {
+				if(data[j]<data[j-1]) {
+					int temp = data[j];
+					data[j] = data[j-1];
+					data[j-1] = temp;
+				}else {
+					break;
+				}
+			}
+		}
+		for(int a : data) {System.out.print(a+" ");}
+	}
+
+}
