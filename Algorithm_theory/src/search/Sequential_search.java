dc87694381981ea36e348ab0b7b315096698ed2a
Jin-Woo Kim <dnehd6739@naver.com>
2020-09-13 10:56:17 +0900

    Binary Search update

--- Algorithm_theory/src/search/Sequential_search.java
+++ Algorithm_theory/src/search/Sequential_search.java
@@ -4,7 +4,18 @@ public class Sequential_search {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] array = new int[] {1,4,2,3,6,7,8,5};
+		sequential_search(array,6);
+	}
+	
+	public static void sequential_search(int[] array, int search) {
+		for(int i=0;i<array.length;i++) {
+			if(array[i]==search) {
+				System.out.println(array[i]+"의 값은"+i+"번째에 있다.");
+			}else {
+				System.out.println("존재 하지 않음");
+			}
+		}
 	}
 
 }
